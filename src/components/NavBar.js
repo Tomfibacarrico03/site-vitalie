866bada073671dcd12f4d74feadca91aa89089dd
tomas-carrico-a22104735 <tomascarrico03@hotmail.com>
2023-10-13 00:32:26 +0100

    home and nav Mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../css/navbar.module.css";
 import { UserAuth } from "../context/AuthContext";
+import Hamburger from 'hamburger-react'
 
 const NavBar = () => {
   const { user } = UserAuth();
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isOpen, setOpen] = useState(false)
 
   useEffect(() => {
     // Update the window width when the component mounts
@@ -26,7 +28,7 @@ const NavBar = () => {
   }, []);
   return (
     <>
-      {windowWidth > 800 ?
+      {windowWidth > 900 ?
         <nav>
           <div className={styles.logo}>
             <Link to="/" style={{ textDecoration: "none" }}>
@@ -83,13 +85,67 @@ const NavBar = () => {
           </ul>
         </nav>
         :
-        <nav>
-          <div className={styles.logo}>
-            <Link to="/" style={{ textDecoration: "none" }}>
-              <h3>meuJob</h3>
-            </Link>
+        <>
+          <nav>
+            <div className={styles.logo}>
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <h3>meuJob</h3>
+              </Link>
+            </div>
+            <div className={styles.HamMenu}>
+              <Hamburger rounded distance="sm" toggled={isOpen} toggle={setOpen} size={25} direction="right" color="#ffff" />
+            </div>
+          </nav>
+          <div className={isOpen ? styles.navOpen : styles.navClose}>
+            <ul>
+              <Link to="/publicar-trabalho" style={{ textDecoration: "none" }}>
+                <li className={styles.btnEntrar}>Criar Trabalho</li>
+              </Link>
+              {user && user.email ? (
+                <>
+                  <Link to="/meustrabalhos" style={{ textDecoration: "none" }}>
+                    <li className={styles.btnPostJob}>Meus trabalhos publicados</li>
+                  </Link>
+                  {user.trade_member == false ? (
+                    <Link
+                      to="/registrar-como-comerciante"
+                      style={{ textDecoration: "none" }}
+                    >
+                      <li className={styles.btnPostJob}>Tornar-se trabalhador</li>
+                    </Link>
+                  ) : (
+                    <Link
+                      to="/dashboard-de-trabalhos/trabalhos-proximos"
+                      style={{ textDecoration: "none" }}
+                    >
+                      <li className={styles.btnPostJob}>Dashboard de trabalhador</li>
+                    </Link>
+                  )}
+
+                  <Link
+                    to="/minha-conta/detalhes-de-contacto"
+                    style={{ textDecoration: "none" }}
+                  >
+                    <li className={styles.btnPostJob}>Minha Conta</li>
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <li className={styles.btnPostJob}>Homeowners</li>
+                  <Link
+                    to="/registrar-como-comerciante"
+                    style={{ textDecoration: "none" }}
+                  >
+                    <li className={styles.btnPostJob}>Tornar-se trabalhador</li>
+                  </Link>
+                  <Link to="/entrar" style={{ textDecoration: "none" }}>
+                    <li className={styles.btnEntrar}>Entrar</li>
+                  </Link>
+                </>
+              )}
+            </ul>
           </div>
-        </nav>
+        </>
       }
     </>
   );
